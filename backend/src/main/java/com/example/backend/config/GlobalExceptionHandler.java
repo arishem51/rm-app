07b065a07f90343c128e71315786ce6175eae9d4
Arishemm <arishem51@gmail.com>
2025-01-17 10:43:08 +0700

    feat: message validate request body

---
+++ backend/src/main/java/com/example/backend/config/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.backend.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.backend.dto.BaseResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<BaseResponse<Map<String, String>>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(
+                new BaseResponse<Map<String, String>>(errors, "Validation failed. Please check the input fields."));
+    }
+}
