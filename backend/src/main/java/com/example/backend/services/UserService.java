4413cd59e9803eaccc89e47507f708e4845feedd
Anhntkhe182290 <anhntkhe182290@fpt.edu.vn>
2025-01-27 01:35:12 +0700

    updateUser

--- backend/src/main/java/com/example/backend/services/UserService.java
+++ backend/src/main/java/com/example/backend/services/UserService.java
@@ -2,11 +2,13 @@ package com.example.backend.services;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.backend.dto.auth.request.SignUpRequest;
+import com.example.backend.dto.auth.request.UpdateUserRequest;
 import com.example.backend.entities.User;
 import com.example.backend.repositories.UserRepository;
 
@@ -45,4 +47,24 @@ public class UserService {
     public Page<User> findUsers(int page, int pageSize) {
         return userRepository.findAll(PageRequest.of(page, pageSize));
     }
+
+    public ResponseEntity<String> updateUser(Long id, UpdateUserRequest request){
+        User user = userRepository.findById(id).orElse(null);
+        if(user == null){
+            return ResponseEntity.status(404).body("User not found");
+        }
+            //Update user's info
+            user.setUsername(request.getName()!= null ? request.getName() : user.getUsername());
+            user.setPassword(request.getPassword()!= null ? request.getPassword() : user.getPassword());
+            user.setPhoneNumber(request.getPhoneNumber()!= null ? request.getPhoneNumber() : user.getPhoneNumber());
+            user.setRole(request.getRole()!= null ? request.getRole() : user.getRole());
+            
+            //Save updated
+            userRepository.save(user);
+
+            return ResponseEntity.ok("User updated successfully.");
+        
+    }
+    
+
 }
