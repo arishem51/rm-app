9a8f8f41515f1e5af33d10df2c27bac005c452e4
Hieu Nguyen <hieunguyenduc0404@gmail.com>
2025-02-09 17:13:46 +0700

    feature/overview-ui

--- frontend/components/dashboard/overview/admin-overview.tsx
+++ frontend/components/dashboard/overview/admin-overview.tsx
@@ -1,6 +1,37 @@
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { Store, UsersRound } from "lucide-react";
+
 const AdminOverview = () => {
-  //FIXME: display data like total users, total shops, total products
-  return <div>Admin Overview</div>;
+  return (
+    <div className="mt-4 flex gap-2">
+      <Card className="flex-1">
+        <CardHeader>
+          <CardTitle className="flex items-center gap-1">
+            <UsersRound size={14} />
+            <span>Total users</span>
+          </CardTitle>
+          {/** FIXME: api */}
+          <CardDescription className="text-neutral-50">0</CardDescription>
+        </CardHeader>
+      </Card>
+      <Card className="flex-1">
+        <CardHeader>
+          <CardTitle className="flex items-center gap-1">
+            <Store size={14} />
+            <span>Total shops</span>
+          </CardTitle>
+          {/** FIXME: api */}
+          <CardDescription className="text-neutral-50">0</CardDescription>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 };
 
 export default AdminOverview;
