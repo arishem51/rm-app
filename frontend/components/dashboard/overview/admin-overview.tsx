155b65f47afe64f290ea446c8868bd8c8810b6a7
Arishemm <arishem51@gmail.com>
2025-02-12 01:31:37 +0700

    feat: data overview admin api

--- frontend/components/dashboard/overview/admin-overview.tsx
+++ frontend/components/dashboard/overview/admin-overview.tsx
@@ -1,33 +1,43 @@
-
 import {
   Card,
   CardHeader,
   CardTitle,
   CardDescription,
 } from "@/components/ui/card";
-import { Store, UsersRound } from "lucide-react";
+import useAppQuery from "@/hooks/use-app-query";
+import { ApiQuery } from "@/services/query";
+import { UsersRound } from "lucide-react";
 
 const AdminOverview = () => {
+  const { data: shopQuery } = useAppQuery(
+    ApiQuery.shops.getShops({ page: 0, search: "" })
+  );
+  const { data: userQuery } = useAppQuery(
+    ApiQuery.users.getUsers({ page: 0, search: "" })
+  );
+
   return (
     <div className="mt-4 flex gap-2">
       <Card className="flex-1">
         <CardHeader>
           <CardTitle className="flex items-center gap-1">
-            <UsersRound size={14} />
+            <UsersRound size={16} />
             <span>Total users</span>
           </CardTitle>
-          {/** FIXME: api */}
-          <CardDescription className="text-neutral-50">0</CardDescription>
+          <CardDescription className="text-neutral-50 text-2xl">
+            {userQuery?.data?.totalElements}
+          </CardDescription>
         </CardHeader>
       </Card>
       <Card className="flex-1">
         <CardHeader>
           <CardTitle className="flex items-center gap-1">
-            <Store size={14} />
+            <UsersRound size={16} />
             <span>Total shops</span>
           </CardTitle>
-          {/** FIXME: api */}
-          <CardDescription className="text-neutral-50">0</CardDescription>
+          <CardDescription className="text-neutral-50 text-2xl">
+            {shopQuery?.data?.totalElements}
+          </CardDescription>
         </CardHeader>
       </Card>
     </div>
