2b2f35db3a8e80178d4c4110c16e4b8fc1b2ea1f
Anhntkhe192290 <Anhntkhe182290fpt.edu.vn>
2025-02-14 00:29:57 +0700

    update-shop

---
+++ backend/src/main/java/com/example/backend/dto/UpdateShopDTO.java
@@ -0,0 +1,10 @@
+package com.example.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateShopDTO {
+    private String shopName;
+    private String shopAddress;
+    private String shopOwner;
+}
