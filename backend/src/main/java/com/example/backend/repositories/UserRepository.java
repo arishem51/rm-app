61450d01689c2717474c2e7ef6c603ce526befac
Arishemm <86037938+arishem51@users.noreply.github.com>
2025-02-21 13:04:46 +0700

    feat: add shop name (#35)

--- backend/src/main/java/com/example/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.backend.entities.Shop;
 import com.example.backend.entities.User;
+import com.example.backend.enums.Role;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
@@ -21,4 +23,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Page<User> findByNameContainingIgnoreCase(String search, PageRequest pageRequest);
 
+    List<User> findByShopAndRole(Shop shop, Role role);
 }
