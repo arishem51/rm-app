168718688a2a0913e8dd1ff8900e9b618025c591
Arishemm <86037938+arishem51@users.noreply.github.com>
2025-02-26 03:29:18 +0700

    chore: feature comming soon inventories (#48)

--- backend/src/main/java/com/example/backend/entities/Shop.java
+++ backend/src/main/java/com/example/backend/entities/Shop.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.UpdateTimestamp;
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
