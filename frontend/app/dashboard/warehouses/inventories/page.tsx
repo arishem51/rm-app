e54bcfd3e8bf6949fc1c56fdfbc4bf8cdebb611f
Arishemm <arishem51@gmail.com>
2025-02-26 03:28:34 +0700

    chore: feature comming soon inventories

--- frontend/app/dashboard/warehouses/inventories/page.tsx
+++ frontend/app/dashboard/warehouses/inventories/page.tsx
@@ -1,20 +1,7 @@
-import HydrationPrefetchQuery from "@/components/dashboard/hydration-prefetch-query";
-import { ApiQuery } from "@/services/query";
+import UnSupportFeature from "@/components/view/un-support-feature";
 
-const WarehousesPage = () => {
-  return (
-    <HydrationPrefetchQuery
-      query={ApiQuery.shops.getShops({ page: 0, search: "" })}
-      awaitQuery
-    >
-      <div className="px-4">
-        <h1 className="text-3xl font-bold mt-2">Inventory management</h1>
-        <p className="text-sm  text-neutral-400 my-1">
-          Manage inventories and their information here.
-        </p>
-      </div>
-    </HydrationPrefetchQuery>
-  );
+const InventoriesPage = () => {
+  return <UnSupportFeature />;
 };
 
-export default WarehousesPage;
+export default InventoriesPage;
