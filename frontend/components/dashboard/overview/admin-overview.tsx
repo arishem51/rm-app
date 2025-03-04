ca9c75eb3c17aae323cbdaca6495ac53588f8455
Arishemm <arishem51@gmail.com>
2025-03-05 03:25:39 +0700

    feat: fake data ui statistic

--- frontend/components/dashboard/overview/admin-overview.tsx
+++ frontend/components/dashboard/overview/admin-overview.tsx
@@ -7,7 +7,7 @@ import {
 import { Skeleton } from "@/components/ui/skeleton";
 import useAppQuery from "@/hooks/use-app-query";
 import { ApiQuery } from "@/services/query";
-import { UsersRound } from "lucide-react";
+import { ShoppingBagIcon, UsersRound } from "lucide-react";
 
 const AdminOverview = () => {
   const { data: shopQuery, isLoading: isShopQueryLoading } = useAppQuery(
@@ -37,7 +37,7 @@ const AdminOverview = () => {
       <Card className="flex-1">
         <CardHeader>
           <CardTitle className="flex items-center gap-1">
-            <UsersRound size={16} />
+            <ShoppingBagIcon size={16} />
             <span>Total shops</span>
           </CardTitle>
           <CardDescription className="text-2xl">
--- frontend/components/dashboard/overview/owner-overview.tsx
+++ frontend/components/dashboard/overview/owner-overview.tsx
@@ -1,6 +1,84 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Box, CreditCard, DollarSign, UsersRound } from "lucide-react";
+import { RevenueChart } from "./revenue-chart";
+import { RecentOrders } from "./recent-orders";
+
 const OwnerOverview = () => {
-  //FIXME: should render data
-  return <div>Owner overview</div>;
+  return (
+    <div>
+      <div className="mt-4 flex gap-4">
+        <Card className="flex-1">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-1">
+              <UsersRound size={16} />
+              <span>Total Staff</span>
+            </CardTitle>
+            <CardDescription className="text-2xl">
+              {false ? <Skeleton className="h-6 w-[100px] mt-1" /> : 0}
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="flex-1">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-1">
+              <Box size={16} />
+              <span>Total Product</span>
+            </CardTitle>
+            <CardDescription className="text-2xl">
+              {false ? <Skeleton className="h-6 w-[100px] mt-1" /> : 0}
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="flex-1">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-1">
+              <DollarSign size={16} />
+              <span>Total Revenue</span>
+            </CardTitle>
+            <CardDescription className="text-2xl">
+              {false ? <Skeleton className="h-6 w-[100px] mt-1" /> : 0}
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="flex-1">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-1">
+              <CreditCard size={16} />
+              <span>Total Debt</span>
+            </CardTitle>
+            <CardDescription className="text-2xl">
+              {false ? <Skeleton className="h-6 w-[100px] mt-1" /> : 0}
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7 mt-4">
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Revenue</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <RevenueChart />
+          </CardContent>
+        </Card>
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Recent Orders</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <RecentOrders />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default OwnerOverview;
