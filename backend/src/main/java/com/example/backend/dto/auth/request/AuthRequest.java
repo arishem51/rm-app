2cae53266faf2c03ea7ca926702ef1d8139847ad
Arishemm <arishem51@gmail.com>
2025-03-11 16:47:53 +0700

    chore: fix auth request

--- backend/src/main/java/com/example/backend/dto/auth/request/AuthRequest.java
+++ backend/src/main/java/com/example/backend/dto/auth/request/AuthRequest.java
@@ -3,11 +3,13 @@ package com.example.backend.dto.auth.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.experimental.SuperBuilder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @SuperBuilder
 @Getter
 @Setter
--- backend/src/main/java/com/example/backend/dto/auth/request/SignInRequest.java
+++ backend/src/main/java/com/example/backend/dto/auth/request/SignInRequest.java
@@ -1,13 +1,17 @@
 package com.example.backend.dto.auth.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Setter
 @Getter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignInRequest extends AuthRequest {
     @NotBlank(message = "ReCaptchaToken is required")
     private String reCaptchaToken;
