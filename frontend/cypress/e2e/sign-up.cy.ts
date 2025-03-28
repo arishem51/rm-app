c3b16c01ba1832f5a134ffbcc449b74dc385fd8a
Arishemm <arishem51@gmail.com>
2025-03-28 13:37:24 +0700

    chore: sign up test

--- frontend/cypress/e2e/sign-up.cy.ts
+++ frontend/cypress/e2e/sign-up.cy.ts
@@ -1,5 +1,9 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Sign Up spec", () => {
+  afterEach(() => {
+    cy.wait(2000);
+  });
+
+  it("Đăng ký với tài khoản đã tồn tại username", () => {
     cy.visit("http://localhost:3000/auth/sign-up");
     cy.get("input[name=phoneNumber]").type("0383906223");
     cy.get("input[name=name]").type("Phùng Văn Hưng");
@@ -9,7 +13,6 @@ describe("template spec", () => {
     cy.get("input[name=confirmPassword]").type("danganh123");
     cy.get("button[type=submit]").should("be.disabled");
 
-    //FAKE to bypass reCAPTCHA
     cy.get("button[type=submit]").then((button) => {
       button[0].removeAttribute("disabled");
     });
@@ -17,5 +20,28 @@ describe("template spec", () => {
     cy.get("button[type=submit]").should("not.be.disabled");
 
     cy.get("button[type=submit]").click();
+    cy.wait(1000);
+    cy.get(".toast-login-error-test").should("be.visible");
+  });
+
+  it("Email không hợp lệ", () => {
+    cy.visit("http://localhost:3000/auth/sign-up");
+    cy.get("input[name=phoneNumber]").type("0383906223");
+    cy.get("input[name=name]").type("Phùng Văn Hưng");
+    cy.get("input[name=email]").type("abc123gmail.com");
+    cy.get("input[name=username]").type("danganh123");
+    cy.get("input[name=password]").type("danganh123");
+    cy.get("input[name=confirmPassword]").type("danganh123");
+    cy.get("button[type=submit]").should("be.disabled");
+
+    cy.get("button[type=submit]").then((button) => {
+      button[0].removeAttribute("disabled");
+    });
+
+    cy.get("button[type=submit]").should("not.be.disabled");
+
+    cy.get("button[type=submit]").click();
+
+    cy.contains("Invalid email").should("be.visible");
   });
 });
